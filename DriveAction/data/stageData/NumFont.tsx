a01ccba1a55d60b8903018e970b3a1b6afa6514c
yanoharuto <8bitharuto@gmail.com>
2023-06-05 09:22:27 +0900

    Commit

---
+++ DriveAction/data/stageData/NumFont.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.11.25" name="NumFont" tilewidth="80" tileheight="100" tilecount="10" columns="10">
+ <image source="../NumFont.png" width="800" height="100"/>
+</tileset>
---
+++ DriveAction/data/stageData/littleRock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.11.25" name="littleRock" tilewidth="208" tileheight="204" tilecount="1" columns="1">
+ <image source="../../../../../../Downloads/littleRock.png" width="208" height="204"/>
+</tileset>
